ced7284ac02615ef80a224cc8d6cc43a5638b157
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-08 18:12:45 -0300

    salvar-exibir-retornar banco-de-dados

--- projeto_cad_usuarios/cad_users/views.py
+++ projeto_cad_usuarios/cad_users/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Usuario #Importa os models da classe
 
 def home(request):
     #Retorna o Arquivo home.html
     return render(request,'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    '''Salvar os dados da tela para o banco de dados.'''
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save() #Salva as informações
+    '''Exibir todos os usuários já cadastrados emuma nova página'''
+    usuarios = {
+        'usuarios' : Usuario.objects.all()
+    }
+    '''Retornar os dados para página de listagem de usuarios'''
+    return render(request,'usuarios/usuarios.html', usuarios)
